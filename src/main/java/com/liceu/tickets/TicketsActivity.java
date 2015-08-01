c2e82af0baa1d5160d12e1612b1c22f66ab4e6df
pnegre <pere.negre@gmail.com>
2015-08-01 02:44:24 +0200

    Anem fent

--- src/main/java/com/liceu/tickets/TicketsActivity.java
+++ src/main/java/com/liceu/tickets/TicketsActivity.java
@@ -3,20 +3,20 @@ package com.liceu.tickets;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.FragmentManager;
-import android.app.ListActivity;
+import android.app.ListFragment;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
-import android.view.*;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.app.ListFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,30 +24,32 @@ import java.util.List;
 public class TicketsActivity extends Activity {
     private DrawerLayout drawerLayout;
     private ListView drawerList;
-    List<String> list;
-    TicketsListFragment lfragment;
+    private List<String> list;
+    private TicketsListFragment lfragment;
 
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.main);
 
+        // Incicialitzem el Drawer (el caixó del menú que es treu fent un swipe a l'esquerra
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
         list = new ArrayList<>();
-        list.add("Preferències");
-        list.add("Refresca");
-        list.add("Nova incidència");
-
+        list.add(getString(R.string.menu_prefs));
+        list.add(getString(R.string.menu_refresh));
+        list.add(getString(R.string.menu_new));
         drawerList = (ListView) findViewById(R.id.left_drawer);
         drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list));
 
+        // Incialitzem i posem en marxa el ListFragment (equivalent a l'Activity abans)
+        // El ListFragment mostra la ListView dels tickets i fa tota la feina (refresc...)
         lfragment = new TicketsListFragment();
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.content_frame, lfragment)
                 .commit();
 
+        // Preparem el Listener pel menú del Drawer
         drawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -57,6 +59,7 @@ public class TicketsActivity extends Activity {
         });
     }
 
+    // Es crida quan triem una opció del Drawer
     private void executeMenu(int position) {
         switch (position) {
             case 0:
@@ -69,14 +72,17 @@ public class TicketsActivity extends Activity {
         }
     }
 
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+    // Això és el ListFragment. Fa tota la feina (refresc, clic per veure ticket...)
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
     public static class TicketsListFragment extends ListFragment {
-
         private TicketsApp tapp;
 
         public TicketsListFragment() {
             // Empty constructor
         }
-
 
         // Called when the activity is first created.
         @Override
@@ -110,7 +116,6 @@ public class TicketsActivity extends Activity {
         private void setMyListAdapter() {
             setListAdapter(new TicketAdapter(getActivity(), R.layout.ticklist_row, tapp.database.getTicketList()));
         }
-
 
 
         // ***********************************************************************
@@ -183,7 +188,6 @@ public class TicketsActivity extends Activity {
                 setMyListAdapter();
             }
         }
-
 
 
     }
